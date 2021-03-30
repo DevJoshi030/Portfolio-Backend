c85bc7edb6b284a28f998c4957ca86abdabb3e2c
Dev Joshi <devjoshi030@gmail.com>
2021-03-30 19:29:09 +0530

    Added GetStockData Method

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
-from .views import GetStockPrice
+from .views import GetStockData, GetStockPrice
 from django.urls import path
 
 urlpatterns = [
     path('get-stock-price/<str:stock>',
          GetStockPrice.as_view(), name='get-stock-price'),
+    path('get-stock-data/<str:stock>',
+         GetStockData.as_view(), name='get-stock-data'),
 ]
--- api/views.py
+++ api/views.py
@@ -16,3 +16,30 @@ class GetStockPrice(APIView):
                             status=status.HTTP_400_BAD_REQUEST)
 
         return Response(data={"price": price}, status=status.HTTP_200_OK)
+
+
+class GetStockData(APIView):
+
+    def get(self, request, format=None, *args, **kwargs):
+
+        stock = kwargs['stock']
+        try:
+            data = si.get_data(ticker=stock, start_date="24/03/2020")
+        except AssertionError:
+            return Response(data={"Error": "No Stock Found!"},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        raw_data = []
+
+        for item in data.itertuples():
+
+            raw_data.append([
+                item[0],
+                str(item[1]),
+                str(item[4]),
+                str(item[2]),
+                str(item[3]),
+                str(item[6])
+            ])
+
+        return Response(data=raw_data, status=status.HTTP_200_OK)
